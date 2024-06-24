7178529afd622f6171b34bc415682b5164a531b7
Ayu <claire@ayu.land>
2024-06-24 22:12:03 +0800

    Running with messages echoed to GDB!

---
+++ fw/debug/run.sh
@@ -0,0 +1,23 @@
+cat >debug/gdbinit <<EOF
+define hook-quit
+  set confirm off
+end
+define hook-run
+  set confirm off
+end
+define hookpost-run
+  set confirm on
+end
+set pagination off
+target extended-remote localhost:3333
+
+b swv_trap_line
+commands
+  silent
+  printf "%s\n", (char *)swv_buf
+  c
+end
+EOF
+
+~/.platformio/packages/toolchain-gccarmnoneeabi/bin/arm-none-eabi-gdb build/app.elf -x debug/gdbinit
+rm debug/gdbinit
--- fw/src/main.c
+++ fw/src/main.c
@@ -1,4 +1,44 @@
 #include "py32f0xx_hal.h"
+#include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#ifndef RELEASE
+static uint8_t swv_buf[256];
+static size_t swv_buf_ptr = 0;
+__attribute__ ((noinline, used))
+void swv_trap_line()
+{
+  *(volatile char *)swv_buf;
+}
+static inline void swv_putchar(uint8_t c)
+{
+  if (c == '\n') {
+    swv_buf[swv_buf_ptr >= sizeof swv_buf ?
+      (sizeof swv_buf - 1) : swv_buf_ptr] = '\0';
+    swv_trap_line();
+    swv_buf_ptr = 0;
+  } else if (++swv_buf_ptr <= sizeof swv_buf) {
+    swv_buf[swv_buf_ptr - 1] = c;
+  }
+}
+static void swv_printf(const char *restrict fmt, ...)
+{
+  char s[256];
+  va_list args;
+  va_start(args, fmt);
+  int r = vsnprintf(s, sizeof s, fmt, args);
+  for (int i = 0; i < r && i < sizeof s - 1; i++) swv_putchar(s[i]);
+  if (r >= sizeof s) {
+    for (int i = 0; i < 3; i++) swv_putchar('.');
+    swv_putchar('\n');
+  }
+}
+#else
+#define swv_printf(...)
+#endif
 
 int main(void)
 {
@@ -14,8 +54,9 @@ int main(void)
   HAL_GPIO_Init(GPIOA, &gpio_init);
 
   while (1) {
-    HAL_Delay(200);
+    HAL_Delay(1000);
     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
+    swv_printf("Hello!\n");
   }
 }
 
