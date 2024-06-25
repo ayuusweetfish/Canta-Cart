225a5b1ba2b7aedd1718aefe07425c96ffbdcf84
Ayu <claire@ayu.land>
2024-06-25 15:31:13 +0800

    Dump readings

--- fw/src/main.c
+++ fw/src/main.c
@@ -40,6 +40,44 @@ static void swv_printf(const char *restrict fmt, ...)
 #define swv_printf(...)
 #endif
 
+#pragma GCC optimize("O3")
+static inline void cap_sense()
+{
+/*
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
+  uint32_t a[100];
+  for (int i = 0; i < 100; i++) {
+    a[i] = GPIOA->IDR & 0x10fd;
+  }
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
+  for (int i = 0; i < 100; i++)
+    if (i == 0 || i + 1 == 100 || a[i] != a[i - 1])
+      swv_printf("%3d %08lx\n", i, a[i]);
+*/
+
+  struct record_t {
+    uint16_t t;
+    uint16_t v;
+  } record[16];
+  int n_records = 0;
+
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 1);
+
+  uint16_t last_v = 0;
+  for (int i = 0; i < 200; i++) {
+    uint32_t a = GPIOA->IDR;
+    uint32_t f = GPIOF->IDR;
+    uint16_t cur_v = (a & 0x10fd) | (f << 6) | last_v;
+    record[n_records] = (struct record_t){.t = i, .v = cur_v};
+    if (last_v != cur_v) n_records++;
+    last_v = cur_v;
+  }
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
+
+  for (int i = 0; i < n_records; i++)
+    swv_printf("%3d %08x\n", record[i].t, record[i].v);
+}
+
 int main(void)
 {
   HAL_Init();
@@ -64,20 +102,38 @@ int main(void)
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  GPIO_InitTypeDef gpio_init = {
-    .Pin = GPIO_PIN_0,
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  GPIO_InitTypeDef gpio_init;
+
+  // BTN_OUT
+  gpio_init = (GPIO_InitTypeDef){
+    .Pin = GPIO_PIN_7,
     .Mode = GPIO_MODE_OUTPUT_PP,
-    .Pull = GPIO_PULLUP,
-    .Speed = GPIO_SPEED_FREQ_HIGH,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
   };
+  HAL_GPIO_Init(GPIOB, &gpio_init);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, 0);
+
+  // BTN_xx (xx = 01, .., 12)
+  gpio_init = (GPIO_InitTypeDef){
+    .Mode = GPIO_MODE_INPUT,
+    .Pull = GPIO_NOPULL,
+    .Speed = GPIO_SPEED_FREQ_VERY_HIGH,
+  };
+  gpio_init.Pin = GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 |
+                  GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_12;
   HAL_GPIO_Init(GPIOA, &gpio_init);
+  gpio_init.Pin = GPIO_PIN_2 | GPIO_PIN_4;
+  HAL_GPIO_Init(GPIOF, &gpio_init);
 
   int i = 0;
   while (1) {
     HAL_Delay(1000);
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
     swv_printf("Hello! %d\n", ++i);
     swv_printf("sys clock = %u\n", HAL_RCC_GetSysClockFreq());
+    cap_sense();
   }
 }
 
