9978aa2cca4e850a3ee4e0fc93e63955a7219e25
Ayu <claire@ayu.land>
2025-08-02 18:01:16 +0800

    fw: Port I2S from Plushies CH57x (WIP)

--- fw/main.c
+++ fw/main.c
@@ -14,6 +14,8 @@ void _putchar(char character)
   R8_UART1_THR = character;
 }
 
+// ============ Touch ============ //
+
 #define TOUCH_HARD_ON_THR 120
 #define TOUCH_SOFT_ON_THR  60
 #define TOUCH_OFF_THR      40
@@ -150,6 +152,116 @@ static inline void cap_sense()
   for (int j = 0; j < 12; j++) printf("%3d%c%c", min(999, cap_sum[j]), btns[j] ? '*' : '.', j == 11 ? '\n' : ' ');
 }
 
+// ============ Audio ============ //
+
+#define AUDIO_BUF_N_BYTES 4000
+static uint8_t audio_buf[AUDIO_BUF_N_BYTES];
+
+static void refill_audio(int half)
+{
+  uint8_t *a = audio_buf + half * (AUDIO_BUF_N_BYTES / 2);
+
+  static int phase = 0;
+  static const int16_t sine_table[] = {
+/*
+import math
+print(','.join('%d' % (4000 * math.sin(i/66. * math.pi * 2)) for i in range(66)))
+*/
+    0,380,757,1126,1486,1832,2162,2472,2760,3022,3258,3464,3638,3780,3887,3959,3995,3995,3959,3887,3780,3638,3464,3258,3022,2760,2472,2162,1832,1486,1126,757,380,0,-380,-757,-1126,-1486,-1832,-2162,-2472,-2760,-3022,-3258,-3464,-3638,-3780,-3887,-3959,-3995,-3995,-3959,-3887,-3780,-3638,-3464,-3258,-3022,-2760,-2472,-2162,-1832,-1486,-1126,-757,-380
+  };
+  for (int i = 0; i < (AUDIO_BUF_N_BYTES / 8); i++) {
+    int16_t sample = sine_table[phase];
+    if ((phase += 3) >= sizeof sine_table / sizeof sine_table[0]) phase = 0;
+    a[i * 4 + 0] = a[i * 4 + 2] = (uint8_t)((sample >> 8) & 0xff);
+    a[i * 4 + 1] = a[i * 4 + 3] = (uint8_t)((sample >> 0) & 0xff);
+  }
+}
+
+// interrupt(WCH-Interrupt-fast") in standard toolchain
+// https://github.com/riscv/riscv-fast-interrupt/issues/329
+__attribute__((section(".highcode")))
+__attribute__((naked))
+void TMR0_IRQHandler_(void)
+{
+  asm volatile ("call TMR0_IRQHandler_actual; mret");
+}
+
+__attribute__((section(".highcode")))
+void TMR0_IRQHandler_actual()
+{
+  TMR0_ClearITFlag(TMR0_3_IT_CYC_END);
+  uint16_t dma_start_addr = (uint16_t)(uintptr_t)audio_buf & ((1 << 14) - 1);
+  uint16_t dma_pos = R16_SPI0_DMA_NOW - dma_start_addr;
+
+  // Refill?
+  static int last_half = 0;
+  int cur_half = (dma_pos >= AUDIO_BUF_N_BYTES / 2);
+  if (last_half != cur_half) {
+    refill_audio(last_half);
+    last_half = cur_half;
+  }
+
+  R16_SPI0_TOTAL_CNT = 4095;  // Keep running
+}
+
+__attribute__((section(".highcode")))
+void TMR0_IRQHandler() { TMR0_IRQHandler_actual(); }
+
+static void i2s_init()
+{
+  // ============ Audio ============ //
+
+  // WS: PA11 TMR2
+  GPIOA_ResetBits(GPIO_Pin_11);
+  GPIOA_ModeCfg(GPIO_Pin_11, GPIO_ModeOut_PP_5mA);
+  GPIOPinRemap(DISABLE, RB_PIN_TMR2);
+  // TMR2_PWMInit(High_Level, PWM_Times_1);
+  R8_TMR2_CTRL_MOD = RB_TMR_OUT_EN | (High_Level << 4) | (PWM_Times_1 << 6);
+  // Fsys = 60 MHz, prescale = 64, bit depth = 16 (2 channels)
+  // -> Fsample = 29.296 kHz
+  int half_period = 64 * 16 + 4;
+    // SCK appears actually 4 SysClock cycles slower, accomodate
+  TMR2_PWMCycleCfg(half_period * 2);
+  TMR2_PWMActDataWidth(half_period);
+
+  // Data output by SPI
+  // BCK: PA13 SCK0
+  // DATA: PA14 MOSI
+  GPIOA_ResetBits(GPIO_Pin_13 | GPIO_Pin_14);
+  GPIOA_ModeCfg(GPIO_Pin_13 | GPIO_Pin_14, GPIO_ModeOut_PP_5mA);
+  GPIOPinRemap(DISABLE, RB_PIN_SPI0);
+  SPI0_MasterDefInit();
+  SPI0_CLKCfg(64);
+  R8_SPI0_CTRL_CFG |= RB_SPI_DMA_LOOP;
+
+  R8_SPI0_CTRL_MOD &= ~RB_SPI_FIFO_DIR;
+  R16_SPI0_DMA_BEG = (uint32_t)audio_buf;
+  R16_SPI0_DMA_END = (uint32_t)(audio_buf + AUDIO_BUF_N_BYTES);
+  R16_SPI0_TOTAL_CNT = 4095;  // Lowest 12 bits effective
+
+__attribute__((noinline))
+void synchronized_enable()
+{
+  __asm__ volatile ("" ::: "memory");
+  R8_TMR2_CTRL_MOD |= RB_TMR_COUNT_EN;  // TMR2_Enable();
+  __asm__ volatile ("" ::: "memory");
+  __asm__ volatile (
+    "nop\n nop\n nop\n nop\n nop\n nop\n nop\n nop\n"
+  );
+  __asm__ volatile ("" ::: "memory");
+  R8_SPI0_CTRL_CFG |= RB_SPI_DMA_ENABLE;
+  __asm__ volatile ("" ::: "memory");
+}
+
+  synchronized_enable();
+
+  TMR0_TimerInit(64 * 16 * 1000);
+  TMR0_ITCfg(ENABLE, TMR0_3_IT_CYC_END);
+  PFIC_EnableIRQ(TMR0_IRQn);
+  // TODO: Use SetVTFIRQ()?
+  // PFIC_FastINT0CFG(TMR0_IRQn, (uint32_t)(uintptr_t)&TMR0_IRQHandler_);
+}
+
 int main()
 {
   PWR_DCDCCfg(DISABLE);
@@ -170,6 +282,8 @@ int main()
   // ACT LED
   GPIOB_SetBits(GPIO_Pin_22);
   GPIOB_ModeCfg(GPIO_Pin_22, GPIO_ModeOut_PP_5mA);
+
+  i2s_init();
 
   // CAP_OUT
   GPIOA_ModeCfg(GPIO_Pin_10, GPIO_ModeOut_PP_5mA);
